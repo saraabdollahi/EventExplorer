e40fc4317339252a0257be0e439a7d74f7623c8c
saraabdollahi <60614048+saraabdollahi@users.noreply.github.com>
2024-02-09 00:35:02 +0100

    Create web_archive_content_preprocessing.py

---
+++ web_archive_content_preprocessing.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from langdetect import detect
+
+entities_df=pd.read_csv("/data/event_aspect_terms.tsv", sep="\t")
+events=["great_fire_of_london"]
+for event in events:
+    entities_tmp=entities_df.loc[entities_df["event_label"]==event,]
+    entities=list(entities_tmp["term"].unique())
+    entities=entities+["when","cause","result","event"]
+    for entity in entities:
+        try:
+            data=pd.read_csv("/data/events/"+event+"/"+entity+".csv",sep=",")
+            data=data.loc[10:,]
+            data.columns=data.iloc[0]
+            data=data[1:].reset_index(drop=True)
+            data["Snippet"]=data["Snippet"].str.replace("<em>","")
+            data["Snippet"]=data["Snippet"].str.replace("</em>","")
+            data["Snippet"]=data["Snippet"].str.replace("</span>","")
+            data["Snippet"]=data["Snippet"].str.replace('<span class="ellipsis">',"")
+            data1=data[["Year","Month","Day","Title","Snippet","Link to archive"]]
+            data=data.loc[data["Snippet"].notna(),]
+            try:
+                data["lang"]=data.apply(lambda row: detect(row.Snippet), axis=1)
+                data=data.loc[data["lang"]=="en"]
+                data.to_csv("/data/events/"+event+"/"+entity+"_results.tsv", sep="\t")
+            except:
+                data["lang"]=""
+                for i in range(data.shape[0]):
+                    try:
+                        lang=detect(data.iloc[i]["Snippet"])
+                        data.loc[i,"lang"]=lang
+                    except:
+                        continue
+                data=data.loc[data["lang"]=="en"]
+                data.to_csv("/data/events/"+event+"/"+entity+"_results.tsv", sep="\t")
+
+        except:
+            print("no data for: ", event, entity)
+
