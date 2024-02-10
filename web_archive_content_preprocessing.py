2758f99053ec566b039304d8e2b502b27affca81
saraabdollahi <60614048+saraabdollahi@users.noreply.github.com>
2024-02-10 02:19:34 +0100

    Update web_archive_content_preprocessing.py

--- web_archive_content_preprocessing.py
+++ web_archive_content_preprocessing.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from langdetect import detect
 
-entities_df=pd.read_csv("/data/event_aspect_terms.tsv", sep="\t")
-events=["great_fire_of_london"]
+entities_df=pd.read_csv("./data/event_aspect_terms.tsv", sep="\t")
+event_ids=pd.read_csv("./data/event_ids.tsv", sep="\t")
+event_ids=event_ids.rename(columns={"eventkg_id":"event", "label":"event_label"})
+entities_df=pd.merge(left=entities_df, right=event_ids, how="left", on="event")
+events=["Great Fire of London"]
 for event in events:
     entities_tmp=entities_df.loc[entities_df["event_label"]==event,]
     entities=list(entities_tmp["term"].unique())
     entities=entities+["when","cause","result","event"]
     for entity in entities:
         try:
-            data=pd.read_csv("/data/events/"+event+"/"+entity+".csv",sep=",")
+            data=pd.read_csv("./data/"+event.lower().replace(" ","_")+"/"+entity+".csv",sep=",")
             data=data.loc[10:,]
             data.columns=data.iloc[0]
             data=data[1:].reset_index(drop=True)
@@ -18,6 +21,7 @@ for event in events:
             data["Snippet"]=data["Snippet"].str.replace("</span>","")
             data["Snippet"]=data["Snippet"].str.replace('<span class="ellipsis">',"")
             data1=data[["Year","Month","Day","Title","Snippet","Link to archive"]]
+            
             data=data.loc[data["Snippet"].notna(),]
             try:
                 data["lang"]=data.apply(lambda row: detect(row.Snippet), axis=1)
@@ -32,7 +36,7 @@ for event in events:
                     except:
                         continue
                 data=data.loc[data["lang"]=="en"]
-                data.to_csv("/data/events/"+event+"/"+entity+"_results.tsv", sep="\t")
+                data.to_csv("./data/"+event.lower().replace(" ","_")+"/"+entity+"_results.tsv", sep="\t")
 
         except:
             print("no data for: ", event, entity)
